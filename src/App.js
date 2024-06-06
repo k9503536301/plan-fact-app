aeca3c9d970f0871d579c2fa93a3dc284e4287c5
Kirill Shurygin <Kirill Shurygin>
2024-06-06 14:16:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import GroupingTable from "./groupingTable";
+import CumulitiveTable from "./cumulitiveTable";
+import FetchData from "./fetchData";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const handleChange = fetchedData => {
+    // let data = fetchedData.map(el => {
+    let tempData = fetchedData.map(el => {
+      let jobDate = new Date(el.jobDate);
+      el.jobYear = jobDate.getFullYear();
+      el.jobMonth = jobDate.toLocaleString("default", { month: "long" });
+      el.jobMonthNumber = jobDate.getMonth();
+      el.sumFact = parseInt(el.sumFact);
+      el.sumPlan = parseInt(el.sumPlan);
+
+      return el;
+    });
+
+    tempData.sort(compareByYear);
+
+    let cumulativeTotalFact = 0,
+      cumulativeTotalPlan = 0;
+    tempData = tempData.map(el => {
+      cumulativeTotalFact += el.sumFact;
+      cumulativeTotalPlan += el.sumPlan;
+      el.cumulativeTotalFact = cumulativeTotalFact;
+      el.cumulativeTotalPlan = cumulativeTotalPlan;
+
+      return el;
+    });
+    setData(tempData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <FetchData onChange={handleChange} />
+      <div style={{ paddingTop: "30px" }}>
+        {data.length > 0 ? <GroupingTable data={data} /> : null}
+      </div>
+
+      <div style={{ paddingTop: "50px" }}>
+        {data.length > 0 ? <CumulitiveTable data={data} /> : null}
+      </div>
     </div>
   );
-}
+};
+
+const compareByYear = (a, b) => {
+  if (a.jobYear < b.jobYear) {
+    return -1;
+  }
+  if (a.jobYear > b.jobYear) {
+    return 1;
+  }
+
+  if (a.jobYear === b.jobYear) {
+    if (a.jobMonthNumber < b.jobMonthNumber) {
+      return -1;
+    }
+    if (a.jobMonthNumber > b.jobMonthNumber) {
+      return 1;
+    }
+  }
+  return 0;
+};
 
 export default App;
---
+++ src/cumulitiveTable.js
@@ -0,0 +1,83 @@
+import { useState, useMemo } from "react";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { Stack } from "@mui/material";
+import DemoRadioGroup from "./demoRadioGroup";
+
+const CumulitiveTable = props => {
+  // const [data, setData] = useState([]);
+  // useEffect(() => setData(fetchedData), [fetchedData]);
+  let data = props.data;
+  console.log("grouping ", props.data[0]);
+  // return null;
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "objectName",
+        header: "Объект",
+      },
+      {
+        accessorKey: "jobName",
+        header: "Вид работ",
+      },
+
+      {
+        accessorKey: "jobDate",
+        header: "Дата",
+      },
+      {
+        accessorKey: "cumulativeTotalPlan",
+        header: "Сумма план",
+      },
+      {
+        accessorKey: "cumulativeTotalFact",
+        header: "Сумма факт",
+      },
+      {
+        accessorKey: "jobYear",
+        header: "Год",
+      },
+      {
+        accessorKey: "jobMonth",
+        header: "Месяц",
+      },
+    ],
+    []
+  );
+
+  const [groupedColumnMode, setGroupedColumnMode] = useState("reorder");
+
+  // const rootData = useMemo(() => data.filter(r => !r.jobYear), [data]);
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    enableGrouping: true,
+    groupedColumnMode,
+    initialState: {
+      expanded: true, //expand all groups by default
+      grouping: ["jobYear", "jobMonth"], //an array of columns to group by by default (can be multiple)
+      pagination: { pageIndex: 0, pageSize: 20 },
+    },
+    muiTableContainerProps: { sx: { maxHeight: "800px" } },
+  });
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center", paddingBottom: "30px" }}>
+        Суммы нарастающим итогом
+      </h2>
+      <Stack gap="1rem">
+        <DemoRadioGroup
+          groupedColumnMode={groupedColumnMode}
+          setGroupedColumnMode={setGroupedColumnMode}
+        />
+        <MaterialReactTable table={table} />
+      </Stack>
+    </div>
+  );
+};
+
+export default CumulitiveTable;
